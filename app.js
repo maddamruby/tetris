e430808eb7dd2bc179fd7af7b2535c6ecd1a9786
Shelyce <srsseymour@gmail.com>
2019-04-10 22:01:36 -0700

    initial commit + shapes

---
+++ app.js
@@ -0,0 +1,38 @@
+
+shapes = [
+    {
+        grid: [2,3],
+        blocks: [[0,0],[0,1],[0,2],[1,2]],
+        name:'lboy',
+    },
+    {
+        grid: [2,2],
+        blocks: [[0,0],[0,1],[1,0],[1,1]],
+        name:'cubey',
+    },
+    {
+        grid: [3,2],
+        blocks:[[0,0],[1,0],[2,0],[1,1]],
+        name:'cactus',
+    },
+    {
+        grid: [1,4],
+        blocks: [[0,0],[0,1][0,2],[0,3]],
+        name:'rod',
+    },
+    {
+        grid:[2,3],
+        blocks:[[0,0],[0,1],[0,2][1,0]],
+        name:'parrot',
+    },
+    {
+        grid:[2,3],
+        blocks: [[0,0][0,1][1,1][1,2]],
+        name:'tweedledee',
+    },
+    {
+        grid:[2,3],
+        blocks: [[1,0][1,1][0,1][0,2]],
+        name:'tweedledum',
+    },
+]
